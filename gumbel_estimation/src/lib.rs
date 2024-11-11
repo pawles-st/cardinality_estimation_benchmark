f1de7f84310c6eac4ce5d8c893cb4de1f7c7d733
Paweł Leśniak <268419@student.pwr.edu.pl>
2024-11-11 18:09:40 +0100

    Implement stochastic processing Gumbel estimation

--- gumbel_estimation/src/lib.rs
+++ gumbel_estimation/src/lib.rs
@@ -1,49 +1,104 @@
-use ahash::{AHasher, RandomState};
-use std::hash::{Hash, Hasher, BuildHasher};
-use std::iter::zip;
+use std::hash::{Hash, BuildHasher};
+use rand::{Rng, thread_rng};
+use rand::distributions::{Uniform};
 
 const NEG_GAMMA: f64 = -0.577215664901532860606512090082402431_f64;
 
-pub struct GumbelEstimator<const K: usize> {
-    observables: [f64; K],
-    hashers: [AHasher; K],
+#[derive(Debug)]
+pub enum GumbelError {
+    InvalidPrecision,
 }
 
-impl<const K: usize> GumbelEstimator<K> {
+/// A cardinality estimator using the Gumbel distribution
+pub struct GumbelEstimator<B: BuildHasher> {
+    builder: B,
+    precision: u8,
+    no_registers: usize,
+    registers: Vec<f32>,
+}
 
-    pub fn new() -> Self {
-        let hash_functions = (0..K).map(|_| {
-            RandomState::new().build_hasher()
-        }).collect::<Vec<_>>().try_into().unwrap();
-        GumbelEstimator{ observables: [f64::NEG_INFINITY; K], hashers: hash_functions }
-    }
+impl<B: BuildHasher> GumbelEstimator<B> {
 
-    pub fn add<T: Hash>(&mut self, value: &T) {
-        let observables_iter = self.observables.iter_mut();
-        let hashers_iter = self.hashers.iter();
-        for (observable, hasher) in zip(observables_iter, hashers_iter) {
-            let gumbel_value = GumbelEstimator::<K>::gen_gumbel(value, hasher);
-            *observable = f64::max(*observable, gumbel_value);
+    /// The minumal accepted precision
+    const MIN_PRECISION: u8 = 4;
+
+    /// The maximmal accepted precision
+    const MAX_PRECISION: u8 = 16;
+
+    /// Creates a new `GumbelEstimator` object with a custom precision and hash builder
+    ///
+    /// # Arguments
+    ///
+    /// - `precision` - corresponds to the number of registers used by this estimator using the 
+    /// formula `no_registers = 2^precision`; the accepted values lie in the range {4, 5, ..., 16}
+    /// - `builder` - this is a hash builder that will be used for hashing provided values
+    pub fn with_precision(precision: u8, builder: B) -> Result<Self, GumbelError> {
+
+        // check if the provided precision is within the bounds
+        if !(Self::MIN_PRECISION..=Self::MAX_PRECISION).contains(&precision) {
+            return Err(GumbelError::InvalidPrecision);
         }
+
+        // calculate the number of registers as `2^precision`
+        let no_registers = 1 << precision;
+
+        // create a uniform [0, 1) rng
+        let mut rng = thread_rng();
+        let unif = Uniform::new(0.0, 1.0);
+
+        // initialise the registers to random gumbel values
+        let registers = (0..no_registers).map(|_| {
+            -f32::ln(-f32::ln(rng.sample(unif)))
+        }).collect();
+
+        // create the estimator object
+        Ok(Self {
+            builder,
+            precision,
+            no_registers,
+            registers,
+        })
     }
 
-    pub fn estimate(&self) -> f64 {
-        let observables_mean = (self.observables.iter().sum::<f64>()) / (self.observables.len() as f64);
-        return f64::exp(NEG_GAMMA + observables_mean);
+    pub fn add<H: Hash + ?Sized>(&mut self, value: &H) {
+        // obtain the value's hash
+        let mut hash = self.builder.hash_one(value) as u32; // TODO: u64
+        
+        // choose a register based on the first `precision` bits
+        let index: usize = (hash >> (32 - self.precision)) as usize;
+
+        // discard the above bits from the hash
+        hash <<= self.precision;
+
+        // create a gumbel random variable
+        let gumbel_value = Self::gen_gumbel(hash);
+
+        // update the register to the max of the gumbel random variables
+        self.registers[index] = f32::max(self.registers[index], gumbel_value);
+        //self.registers.set_greater(register_index, gumbel_value);
     }
 
-    fn gen_gumbel<T: Hash>(value: &T, hasher: &AHasher) -> f64 {
-        let mut state = hasher.clone();
-        value.hash(&mut state);
-        let hash = state.finish();
+    pub fn count(&self) -> f64 {
+        // calculate the mean of the registers
+        let registers_mean = self.registers.iter().map(|&val| val as f64).sum::<f64>() / (self.no_registers as f64);
+        
+        // return the cardinality estimate
+        self.no_registers as f64 * f64::exp(NEG_GAMMA + registers_mean)
+    }
 
-        let exponent = 1023;
-        let mantissa = hash & ((1u64 << 52) - 1);
+    #[inline(always)]
+    fn gen_gumbel(hash: u32) -> f32 {
+        // create the exponent and mantissa bits
+        let exponent_bits = 127 << 23;
+        let mantissa_bits = hash >> 9;
 
-        let f64_bits = (exponent << 52) | mantissa;
-        let random_unif = f64::from_bits(f64_bits) - 1.0;
+        // combine the bits
+        let bits = exponent_bits | mantissa_bits;
 
-        return -f64::ln(-f64::ln(random_unif));
+        // create a random [0, 1) float
+        let random_unif = f32::from_bits(bits) - 1.0;
+
+        // create a gumbel random variable
+        -f32::ln(-f32::ln(random_unif))
     }
 }
-
