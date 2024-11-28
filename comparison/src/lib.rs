35c9bf9ce7c87137d12783bb5e1277c544b92566
Paweł Leśniak <268419@student.pwr.edu.pl>
2024-11-28 13:34:04 +0100

    Remove GHLLS

--- comparison/src/lib.rs
+++ comparison/src/lib.rs
@@ -33,18 +33,15 @@ pub fn gather(prec: u8, card: usize, size: usize) -> Result<(), io::Error> {
 
     // prepare the output
     let mut hll_out = create_output("HLL", prec, card, size)?;
-    let mut ghll_geo_out = create_output("GHLL_Geo", prec, card, size)?;
-    let mut ghll_har_out = create_output("GHLL_Har", prec, card, size)?;
+    let mut ghll_geo_out = create_output("GHLLGeo", prec, card, size)?;
+    let mut ghll_har_out = create_output("GHLLHar", prec, card, size)?;
     let mut ghllp_out = create_output("GHLLPlus", prec, card, size)?;
-    let mut ghlls_out = create_output("GHLLS", prec, card, size)?;
     
     // create `ITERATIONS` independent estimators with a common random state
     let builders: Vec<_> = (0..ITERATIONS).map(|_| RandomState::new()).collect();
     let mut hll_estimators: Vec<_> = (0..ITERATIONS).map(|i| HyperLogLogPF::<u64, _>::new(prec, builders[i].clone()).unwrap()).collect();
     let mut ghll_estimators: Vec<_> = (0..ITERATIONS).map(|i| GHLL::<_>::with_precision(prec, builders[i].clone()).unwrap()).collect();
     let mut ghllp_estimators: Vec<_> = (0..ITERATIONS).map(|i| GHLLPlus::<_>::with_precision(prec, builders[i].clone()).unwrap()).collect();
-    let mut ghlls_estimators: Vec<_> = (0..ITERATIONS).map(|i| GHLLS::<_>::with_precision(prec, builders[i].clone()).unwrap()).collect();
-
 
     // analyse the data
     for line in reader.lines() {
@@ -63,9 +60,6 @@ pub fn gather(prec: u8, card: usize, size: usize) -> Result<(), io::Error> {
         for estimator in &mut ghllp_estimators {
             estimator.add(&value);
         };
-        for estimator in &mut ghlls_estimators {
-            estimator.add(&value);
-        };
     };
 
     // acquire the cardinality estimate for each estimator and write the result
@@ -82,10 +76,6 @@ pub fn gather(prec: u8, card: usize, size: usize) -> Result<(), io::Error> {
     for estimator in &mut ghllp_estimators {
         let estimate = estimator.count();
         writeln!(ghllp_out, "{}", estimate)?;
-    }
-    for estimator in &mut ghlls_estimators {
-        let estimate = estimator.count();
-        writeln!(ghlls_out, "{}", estimate)?;
     }
 
     Ok(())
--- gumbel_estimation/src/lib.rs
+++ gumbel_estimation/src/lib.rs
@@ -3,9 +3,7 @@ mod gen_gumbel;
 mod registers;
 
 pub mod ghll;
-pub mod ghlls;
 pub mod ghll_plus;
 
 pub use ghll::GHLL;
-pub use ghlls::GHLLS;
 pub use ghll_plus::GHLLPlus;
