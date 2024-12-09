f4ef87a1b84d4566cc4b0d2524e067b52acdee77
Paweł Leśniak <268419@student.pwr.edu.pl>
2024-12-09 23:26:13 +0100

    Add all graph generators

--- visualisation/boxplot.R
+++ visualisation/boxplot.R
@@ -2,7 +2,7 @@ source("common.R")
 
 # datasets config
 
-card <- 100000
+card <- 500000
 mult <- 100
 precisions <- c(4, 8, 12, 16)
 iters <- 100
@@ -10,6 +10,7 @@ iters <- 100
 # algorithms to analyse
 
 algorithms <- c("HLL", "GHLLGeo", "GHLLHar", "GHLLPlus")
+algorithms.readable <- c("HyperLogLog", "GumbelHyperLogLog (średnia geometryczna)", "GumbelHyperLogLog (średnia harmoniczna)", "GumbelHyperLogLog+")
 no.algorithms <- length(algorithms)
 
 # create the plots
@@ -22,20 +23,27 @@ for (prec in precisions) {
 
 	for (i in 1:no.algorithms) {
 		alg.estimates <- read.data(algorithms[i], prec, card, mult)
-		estimates <- c(estimates, list(alg.estimates))
+		estimates <- c(estimates, list(alg.estimates / card))
 	}
 
 	# create a comparison boxplot
 
 	png(paste0("boxplot_", prec, ".png"), width = 1920, height = 1080)
+	
+	par(mar = c(6, 6, 4, 2))
 
 	boxplot(estimates,
-		main = paste("Boxplots for cardinality estimators (cardinality = ", card, ")"),
-		xlab = "Algorithm",
-		ylab = "Estimations",
+		main = paste0("Wykres pudełkowy dla algorytmów (n = ", format(card, scientific = FALSE), ", k = ", 2^prec , ")"),
+		xlab = "",
+		ylab = "",
 		names = algorithms,
-		col = rainbow(no.algorithms)
+		col = rainbow(no.algorithms),
+		cex.main = 3,
+		cex.axis = 2.5
 	)
+	
+	title(xlab = "Algorytm", line = 3.5, cex.lab = 3)
+	title(ylab = "Estymacja / Liczba unikalnych elementów", line = 3.5, cex.lab = 3)
 
 	# mark the means
 
@@ -44,11 +52,18 @@ for (prec in precisions) {
 
 	# mark the cardinality with a line
 
-	abline(h = card, lwd = 2, col = 'green')
+	abline(h = 1, lwd = 2)
 
 	# add a legend
 
-	legend("topright", legend = algorithms, fill = rainbow(no.algorithms), bty = "n", cex = 1.5)
+	legend("topleft",
+	       legend = algorithms.readable,
+	       fill = rainbow(no.algorithms),
+	       bty = "n",
+	       cex = 2,
+	       bg = "white",
+	       box.lwd = 1,
+	)
 
 	dev.off()
 }
